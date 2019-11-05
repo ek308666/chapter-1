fd85d585709c2bef46f669f929d4f82f8521703a
ek308666 <57112503+ek308666@users.noreply.github.com>
2019-11-05 14:06:57 -0600

    update chapter 1

---
+++ mug/mug/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating a mug
+            Console.WriteLine("HHHHHHHHHHHHHHHHHHHHHHHHHHH");
+            Console.WriteLine("H                         H");
+            Console.WriteLine("H                         H");
+            Console.WriteLine("H                         H");
+            Console.WriteLine("H                         HHH");
+            Console.WriteLine("H                         H  H");
+            Console.WriteLine("H                         H   H");
+            Console.WriteLine("H                         H    H");
+            Console.WriteLine("H                         H      H");
+            Console.WriteLine("H                         H       H");
+            Console.WriteLine("H                         H       H");
+            Console.WriteLine("H                         H       H");
+            Console.WriteLine("H                         H       H");
+            Console.WriteLine("H                         H      H");
+            Console.WriteLine("H                         H     H");
+            Console.WriteLine("H                         H    H");
+            Console.WriteLine("H                         H   H");
+            Console.WriteLine("H                         H  H");
+            Console.WriteLine("H                         H H");
+            Console.WriteLine("H                         HH");
+            Console.WriteLine("H                         H");
+            Console.WriteLine("HHHHHHHHHHHHHHHHHHHHHHHHHHH");
+        }
+    }
+}
---
+++ wineglass/wineglass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wineglass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating a wine glass graphic
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("X                     X");
+            Console.WriteLine(" X                   X");
+            Console.WriteLine("  XXXXXXXXXXXXXXXXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("          XXXXX");
+            Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXX");
+
+        }
+    }
+}
